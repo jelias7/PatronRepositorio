59b5948beb389c74192a7899926567e53aba5d26
Juan Rosario <juanelias18@hotmail.com>
2019-06-21 23:32:58 -0400

    Tests de Usuarios hechos

---
+++ PatronRepositorioTests/BLL/UsuariosTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class UsuariosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuarios usuarios = new Usuarios();
+            usuarios.Idusuario = 1;
+            usuarios.Idempleado = 1;
+            usuarios.Nombreusuario = "Carlos";
+            usuarios.Clave = "ll";
+
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            paso = repositorio.Guardar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            Usuarios usuarios = repositorio.Buscar(1);
+            usuarios.Nombreusuario = "pepe";
+            paso = repositorio.Modificar(usuarios);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            Usuarios usuarios = repositorio.Buscar(1);
+            Assert.IsNotNull(usuarios);
+        }
+
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            List<Usuarios> lista = new List<Usuarios>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
+    }
+}
