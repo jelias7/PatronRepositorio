6a9d8f8e8722a733221083f76b4e892f48e214ae
Juan Rosario <juanelias18@hotmail.com>
2019-06-21 21:02:06 -0400

    Tests de Clientes hecho

---
+++ PatronRepositorioTests/BLL/ClientesTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class ClientesTests
+    {
+        [TestMethod]
+        public void GuardarTest()
+        {
+            Clientes c = new Clientes();
+            c.Idcliente = 1;
+            c.Nombre = "Juan";
+            c.Direccion = "LOLO";
+
+            RepositorioBase<Clientes> repositorio = new RepositorioBase<Clientes>();
+            bool paso = false;
+            paso = repositorio.Guardar(c);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Clientes> repositorio = new RepositorioBase<Clientes>();
+            bool paso = false;
+            Clientes c = repositorio.Buscar(1);
+            c.Nombre = "Jose";
+            paso = repositorio.Modificar(c);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Clientes> repositorio = new RepositorioBase<Clientes>();
+            Clientes c = repositorio.Buscar(1);
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Clientes> repositorio = new RepositorioBase<Clientes>();
+            List<Clientes> lista = new List<Clientes>();
+            lista = repositorio.GetList(c => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Clientes> db = new RepositorioBase<Clientes>();
+
+            Assert.IsTrue(db.Eliminar(1));
+        }
+    }
+}
