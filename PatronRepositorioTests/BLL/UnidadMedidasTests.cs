6859d0b65ba770fad7291eb1f7ac0708d5413d2e
Juan Rosario <juanelias18@hotmail.com>
2019-06-21 23:28:37 -0400

    Tests de UnidadMedidas y Usuario_has_permiso hechos

---
+++ PatronRepositorioTests/BLL/UnidadMedidasTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class UnidadMedidasTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Unidadmedidas u = new Unidadmedidas();
+            u.Idunidadmedida = 1;
+            u.Nombreunidad = "Kilos";
+
+            RepositorioBase<Unidadmedidas> repositorio = new RepositorioBase<Unidadmedidas>();
+            bool paso = false;
+            paso = repositorio.Guardar(u);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Unidadmedidas> repositorio = new RepositorioBase<Unidadmedidas>();
+            Unidadmedidas u = repositorio.Buscar(1);
+            u.Nombreunidad = "Kilogramos";
+            bool paso = false;
+            paso = repositorio.Modificar(u);
+            Assert.AreEqual(true, paso);
+        }
+
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Unidadmedidas> repositorio = new RepositorioBase<Unidadmedidas>();
+            Unidadmedidas u = repositorio.Buscar(1);
+            Assert.IsNotNull(u);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Unidadmedidas> repositorio = new RepositorioBase<Unidadmedidas>();
+            List<Unidadmedidas> lista = new List<Unidadmedidas>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Unidadmedidas> repositorio = new RepositorioBase<Unidadmedidas>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
+    }
+}
---
+++ PatronRepositorioTests/BLL/Usuario_has_permisoTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class Usuario_has_permisoTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuario_has_permiso u = new Usuario_has_permiso();
+            u.Idusuario = 1;
+            u.Idpermiso = 1;
+
+            RepositorioBase<Usuario_has_permiso> repositorio = new RepositorioBase<Usuario_has_permiso>();
+            bool paso = false;
+            paso = repositorio.Guardar(u);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Usuario_has_permiso> repositorio = new RepositorioBase<Usuario_has_permiso>();
+            bool paso = false;
+            Usuario_has_permiso u = repositorio.Buscar(1);
+            u.Idpermiso = 11;
+            paso = repositorio.Modificar(u);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Usuario_has_permiso> repositorio = new RepositorioBase<Usuario_has_permiso>();
+            Usuario_has_permiso u = repositorio.Buscar(1);
+            Assert.IsNotNull(u);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Usuario_has_permiso> repositorio = new RepositorioBase<Usuario_has_permiso>();
+            List<Usuario_has_permiso> lista = new List<Usuario_has_permiso>();
+            lista = repositorio.GetList(u => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Usuario_has_permiso> repositorio = new RepositorioBase<Usuario_has_permiso>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
