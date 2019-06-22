0e38d93abebef14b130ef3bafa5f9975223d497d
Juan Rosario <juanelias18@hotmail.com>
2019-06-21 23:02:31 -0400

    Tests de Proveedores y Productos hechos

---
+++ PatronRepositorioTests/BLL/ProductosTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class ProductosTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Productos p = new Productos();
+
+            p.Idproducto = 1;
+            p.Nombre = "Juan";
+            p.Descripcion = "lololo";
+            p.Fechafabricacion = DateTime.Now;
+            p.Costocompra = 2;
+            p.Stock = 10;
+            p.Idunidadmedida = 1;
+            p.Idimagen = 1;
+            p.Idcategoria = 1;
+            p.Idmarca = 1;
+            p.Idmodelo = 1;
+
+            RepositorioBase<Productos> repositorio = new RepositorioBase<Productos>();
+            bool paso = false;
+            paso = repositorio.Guardar(p);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Productos> repositorio = new RepositorioBase<Productos>();
+            bool paso = false;
+            Productos p = repositorio.Buscar(1);
+            p.Nombre = "Jose";
+            paso = repositorio.Modificar(p);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Productos> repositorio = new RepositorioBase<Productos>();
+            Productos p = repositorio.Buscar(1);
+            Assert.IsNotNull(p);
+        }
+
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Productos> repositorio = new RepositorioBase<Productos>();
+            List<Productos> lista = new List<Productos>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Productos> repositorio = new RepositorioBase<Productos>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
---
+++ PatronRepositorioTests/BLL/ProvedoresTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class ProvedoresTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Provedores p = new Provedores();
+            p.Idproveedor = 1;
+            p.Idpersona = 1;
+
+            RepositorioBase<Provedores> repositorio = new RepositorioBase<Provedores>();
+            bool paso = false;
+            paso = repositorio.Guardar(p);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Provedores> repositorio = new RepositorioBase<Provedores>();
+            bool paso = false;
+            Provedores p = repositorio.Buscar(1);
+            p.Idpersona = 11;
+            paso = repositorio.Modificar(p);
+            Assert.AreEqual(true, paso);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Provedores> repositorio = new RepositorioBase<Provedores>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Provedores> repositorio = new RepositorioBase<Provedores>();
+            Provedores p = repositorio.Buscar(1);
+            Assert.IsNotNull(p);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Provedores> repositorio = new RepositorioBase<Provedores>();
+            List<Provedores> lista = new List<Provedores>();
+            lista = repositorio.GetList(p => true);
+            Assert.IsNotNull(lista);
+        }
+
+    }
+}
