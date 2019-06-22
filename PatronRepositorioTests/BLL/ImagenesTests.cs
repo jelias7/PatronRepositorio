05343415c9ac56c7a62e4eaf0f9ef4151aa6603c
Juan Rosario <juanelias18@hotmail.com>
2019-06-21 22:16:27 -0400

    Tests de Imagenes y Marcas hechos

---
+++ PatronRepositorioTests/BLL/ImagenesTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class ImagenesTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Imagenes i = new Imagenes();
+            i.Idimagen = 1;
+            i.Rutaimagen = "lolo";
+
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            paso = repositorio.Guardar(i);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            Imagenes i = repositorio.Buscar(1);
+            i.Rutaimagen = "xlxlx";
+            paso = repositorio.Modificar(i);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            Imagenes i = repositorio.Buscar(1);
+            Assert.IsNotNull(i);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            List<Imagenes> lista = new List<Imagenes>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Imagenes> repositorio = new RepositorioBase<Imagenes>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
---
+++ PatronRepositorioTests/BLL/MarcasTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorio.BLL;
+using PatronRepositorio.Entidades;
+
+namespace PatronRepositorioTests.BLL
+{
+    [TestClass]
+    public class MarcasTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Marcas m = new Marcas();
+            m.Idmarca = 1;
+            m.Nombremarca = "Juan";
+
+            RepositorioBase<Marcas> repositorio = new RepositorioBase<Marcas>();
+            bool paso = false;
+            paso = repositorio.Guardar(m);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            RepositorioBase<Marcas> repositorio = new RepositorioBase<Marcas>();
+            bool paso = false;
+            Marcas m = repositorio.Buscar(1);
+            m.Nombremarca = "Elias";
+            paso = repositorio.Modificar(m);
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            RepositorioBase<Marcas> repositorio = new RepositorioBase<Marcas>();
+            Marcas m = repositorio.Buscar(1);
+            Assert.IsNotNull(m);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            RepositorioBase<Marcas> repositorio = new RepositorioBase<Marcas>();
+            List<Marcas> lista = new List<Marcas>();
+            lista = repositorio.GetList(e => true);
+            Assert.IsNotNull(lista);
+        }
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            RepositorioBase<Marcas> repositorio = new RepositorioBase<Marcas>();
+            bool paso = false;
+            paso = repositorio.Eliminar(1);
+            Assert.AreEqual(true, paso);
+        }
+    }
+}
